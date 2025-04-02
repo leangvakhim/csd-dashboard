670adb8d18735d4fb5ec3c959b9579d8b63547e7
Khim070 <Vakhiml60@gmail.com>
2025-04-02 13:35:08 +0700

    init

--- src/service/APIConfig.jsx
+++ src/service/APIConfig.jsx
@@ -2,18 +2,20 @@ const API_BASEURL = "https://54.151.188.108/api";
 
 const API_ENDPOINTS = {
     // text
-    texts: `${API_BASEURL}/text`,
+    // texts: `${API_BASEURL}/text`,
 
     // images
     getImages: `${API_BASEURL}/images`,
     uploadImage: `${API_BASEURL}/images/create`,
     deleteImage: `${API_BASEURL}/images/delete`,
 
+    // news
+
     // button
-    buttons: `${API_BASEURL}/button`,
+    // buttons: `${API_BASEURL}/button`,
 
     // addon
-    addons: `${API_BASEURL}/addon`
+    // addons: `${API_BASEURL}/addon`
 };
 
 export {API_BASEURL, API_ENDPOINTS};
