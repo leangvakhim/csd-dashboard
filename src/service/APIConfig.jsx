f65ccb777a7f4eb39e73d5dbf90092cd2d3a5903
Khim070 <Vakhiml60@gmail.com>
2025-04-16 22:02:24 +0700

    Update APIConfig.jsx

--- src/service/APIConfig.jsx
+++ src/service/APIConfig.jsx
@@ -1,5 +1,5 @@
-// const API = "https://52.221.196.157";
-const API = "http://127.0.0.1:8000";
+const API = "https://52.221.196.157";
+// const API = "http://127.0.0.1:8000";
 const API_BASEURL = `${API}/api`;
 
 const API_ENDPOINTS = {
@@ -162,7 +162,7 @@ const API_ENDPOINTS = {
     deleteScholarship: `${API_BASEURL}/scholarship/visibility`,
     duplicateScholarship: `${API_BASEURL}/scholarship/duplicate`,
     updateScholarshipOrder: `${API_BASEURL}/scholarship/reorder`,
-    
+
 
     getResearch: `${API_BASEURL}/rsd`,
     createResearch: `${API_BASEURL}/rsd/create`,
