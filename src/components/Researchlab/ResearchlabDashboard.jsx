e2487f8ebd2670f6b80a1c8fb111b21615089bb5
Khim070 <Vakhiml60@gmail.com>
2025-04-24 10:57:56 +0700

    page and researchlab

--- src/components/Researchlab/ResearchlabDashboard.jsx
+++ src/components/Researchlab/ResearchlabDashboard.jsx
@@ -8,6 +8,7 @@ const ResearchlabDashboard = () => {
     const [activeDropdown, setActiveDropdown] = useState(null);
     const [researchlabItems, setResearchlabItems] = useState([]);
     const navigate = useNavigate();
+    const [dropdownPosition, setDropdownPosition] = useState({ top: 0, left: 0 });
 
     useEffect(() => {
         const fetchEvents = async () => {
@@ -92,7 +93,7 @@ const ResearchlabDashboard = () => {
 
     return (
         <div className="relative overflow-x-auto shadow-md px-8">
-            <table className="w-full text-sm text-left border border-gray-200 text-gray-500 ">
+            <table className="w-full text-sm text-left border border-gray-200 text-gray-500 mb-32">
                 <thead className="text-xs text-gray-700  uppercase bg-gray-50 ">
                     <tr>
                         <th scope="col" className="px-6 py-3">
@@ -138,13 +139,22 @@ const ResearchlabDashboard = () => {
                                     </a> |
                                     <div className="relative">
                                         <button
-                                            onClick={() => setActiveDropdown(activeDropdown === item.rsdl_id ? null : item.rsdl_id)}
+                                            onClick={(e) => {
+                                                const button = e.currentTarget;
+                                                const parent = button.offsetParent;
+                                                const rect = button.getBoundingClientRect();
+                                                const parentRect = parent.getBoundingClientRect();
+                                                setDropdownPosition({ top: rect.bottom - parentRect.top, left: rect.left - parentRect.left });
+                                                setActiveDropdown(activeDropdown === item.rsdl_id ? null : item.rsdl_id);
+                                            }}
                                             className="font-medium text-gray-900 hover:text-blue-500"
                                         >
                                             <i className="ti ti-dots-vertical text-xl"></i>
                                         </button>
                                         {activeDropdown === item.rsdl_id && (
-                                            <div className="fixed right-0 mt-2 w-36 mr-8 bg-white border border-gray-300 rounded-md shadow-md z-50">
+                                            <div
+                                                style={{ position: 'absolute', top: `${dropdownPosition.top}px`, left: `${dropdownPosition.left}px` }}
+                                                className="absolute w-36 bg-white border border-gray-300 rounded-md shadow-md max-h-48 z-50 overflow-y-auto">
                                                 <div className="py-1">
                                                     <a
                                                         onClick={() => handleEdit(item.rsdl_id)}
